b7193a80700d23cad096c656c85393ab26845acf
Ankus Nayak <47305497+ankusnayak@users.noreply.github.com>
2023-03-09 13:43:05 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,26 +1,124 @@
 import json
 import boto3
 import urllib
+import os
+import sys
+from botocore.exceptions import ClientError
+from datetime import datetime
 
-def debugging(msg):
+
+class Tools:
     
-    try:
-        if msg:
-            print(msg)
-        else:
-            print("Empty Message")
+    @staticmethod
+    def debugging(*msg):
+        try:
+            if msg:
+                for m in msg:
+                    print(m)
+            else:
+                print("Empty Message")
+            # input()
             
-        # input()
+        except Exception as e:
+            print("Error:: %s"%str(e))
+            sys.exit()
         
-    except Exception as e:
-        print("Error:: %s"%str(e))
-        sys.exit()
+    @staticmethod
+    def get_datetime():
+        
+        # return a current datetime string 
+        return datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    
+
+class EmailOperation:
+    
+    def __init__(self,status='success'):
+        self.ses_client = self.sesClientObject()
+        self.template_name = status+"_template"
+        
+        
+    def sesClientObject(self):
+        try:
+            return boto3.client('ses')
+        except ClientError as e:
+            print("Error: while create boto3 sns client \n %s"%str(e))
+            
+        
+    def createAndUpdateTemplate(self):
+        
+        # self.ses_client.delete_template(
+        #     TemplateName = self.template_name
+        # )
+        
+        try:
+            
+            response=self.ses_client.get_template(TemplateName=self.template_name)
+            if response['Template'] and response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                self.ses_client.update_template(
+                        Template={
+                            "TemplateName": self.template_name,
+                            "SubjectPart": "{{status}}:File Name: {{file_name}} || Date: {{date}}",
+                            # "SubjectPart": "Subject",
+                            "HtmlPart": "Hello Ankus HTML",
+                            "TextPart": "Hello Ankus Text"
+                        }
+                    )
+                
+        except ClientError as e:
+            
+            self.ses_client.create_template(
+                    Template={
+                        # "TemplateName": self.template_name,
+                        "SubjectPart": "{{status}}:File Name: {{file_name}} || Date: {{date}}",
+                        "SubjectPart": "Subject",
+                        "HtmlPart": "Hello Ankus HTML",
+                        "TextPart": "Hello Ankus Text"
+                    }
+                )
+        else:
+            return True
+
+    def mailStatus(self,mail_response):
+
+        if mail_response:
+            if mail_response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                print('email sent successfully..')
+                # return True
+            else:
+                print('email sending failed..')
+                # return False
+        else:
+            print('email response is empty...')
+            # return False
+        
+    def sendEmail(self,source_email,dest_email,mail_data):
+        
+        response = None
+        
+        try:
+            response = self.ses_client.send_templated_email(
+                    Source = source_email,
+                    Destination={
+                        'ToAddresses': [
+                            dest_email
+                        ],
+                        # 'CcAddresses': ['iamankus7@gmail.com']
+                    },
+                    Template=self.template_name,
+                    # TemplateData = mail_data
+                    TemplateData = mail_data
+                )
+                
+            return response
+        except Exception as e:
+            print("Error: %s"%str(e))
+        
+
 
 def lambda_handler(event, context):
     
     s3_client = boto3.client('s3')
-    sns_client = boto3.client('sns')
-    
     
     if event:
         file_obj = event['Records'][0]
@@ -29,12 +127,18 @@ def lambda_handler(event, context):
         key = file_obj['s3']['object']['key']
         key = urllib.parse.unquote_plus(key,encoding='utf-8')
         
+        # - Need to find better approach for assign...
+        file_name,file_prefix,file_type = None, None, None
         
-        file_name = key.split('/')[-1]
-        # file_prefix = key.split('/')[-2]
-        file_type = file_name.split('.')[-1]
+        if os.path.dirname(key):
+            file_name = key.split('/')[-1]
+            file_prefix = key.split('/')[-2]
+            file_type = file_name.split('.')[-1]
+        else:
+            file_name = key
+            file_type = key.split('.')[-1]
         
-        debugging(key)
+        # Tools.debugging(file_name,file_prefix,file_type)
         
         
         
@@ -46,3 +150,14 @@ def lambda_handler(event, context):
         #     print("Error: %s" %str(e))
         #     # raise e
         
+        
+        ##################Testing###################
+        
+        test_mail_data = "{"status": 'success',"file_name": str(file_name),"date": str(Tools.get_datetime())}"
+
+        
+        emailObj = EmailOperation('success')
+        
+        if emailObj.createAndUpdateTemplate():
+            mailResponse = emailObj.sendEmail('iamankus7@gmail.com','iamankus7@gmail.com',test_mail_data)
+            emailObj.mailStatus(mailResponse)
